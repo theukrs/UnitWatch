21a996fd29ce139e5f3c703d814a22487acf4795
UkRs <theukrs@gmail.com>
2026-08-29 20:57:16 +0500

    feat: add import/export buttons (work in progress)

--- widgets/settings.py
+++ widgets/settings.py
@@ -1,12 +1,20 @@
-from PyQt6.QtWidgets import QDialog, QFormLayout, QSpinBox, QPushButton
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDialog, QFormLayout, QSpinBox, QPushButton, QHBoxLayout, QSizePolicy
 import duckdb
 green_btn_style = """
-QPushButton {background-color: #27ae60;font-size: 15pt;border-radius: 8px;padding:5px;}
+QPushButton {background-color: #27ae60;font-size: 15pt;border-radius: 2px;padding:5px;}
 QPushButton:hover {background-color: #219150;}
 """
 red_btn_style = """
-QPushButton {background-color: #e74c3c; color: white;font-size: 15pt; border-radius: 8px; padding: 5px;} 
+QPushButton {background-color: #e74c3c; font-size: 15pt; border-radius: 2px; padding: 5px;} 
 QPushButton:hover { background-color: #c0392b;}
+"""
+blue_btn_style = """
+QPushButton {background-color: #1f4e6e;font-size: 11pt;border-radius: 12px;padding: 5px; }
+QPushButton:hover {background-color: #286486;}"""
+purple_btn_style = """
+QPushButton {background-color: #176b5b;font-size: 11pt;border-radius: 12px;padding: 5px; } 
+QPushButton:hover { background-color: #20816e;}
 """
 class Settings(QDialog):
     def __init__(self,parent=None):
@@ -24,23 +32,44 @@ class Settings(QDialog):
         self.reading_day = QSpinBox()
         self.reading_day.setRange(1,28)
 
+        self.import_btn = QPushButton('Import')
+        self.import_btn.setStyleSheet(blue_btn_style)
+        self.import_btn.se
+
+        self.export_btn = QPushButton('Export')
+        self.export_btn.setStyleSheet(purple_btn_style)
+
         self.save_btn = QPushButton('Save')
         self.save_btn.setStyleSheet(green_btn_style)
-        self.save_btn.setMinimumWidth(100)
+
         self.cancel_btn = QPushButton('Cancel')
         self.cancel_btn.setStyleSheet(red_btn_style)
-        self.cancel_btn.setMinimumWidth(100)
+
+        for btn in (self.import_btn, self.export_btn):
+            btn.setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
 
     def create_grid(self):
         layout = QFormLayout()
-        layout.addRow('Units:',self.units_limit)
+
+        layout.setVerticalSpacing(15)
+        layout.setHorizontalSpacing(20)
+
+        layout.addRow('Units:', self.units_limit)
         layout.addRow('Reading Date:',self.reading_day)
-        layout.addRow(self.save_btn,self.cancel_btn)
+        layout.addRow('Import Readings:',self.import_btn)
+        layout.addRow('Export Readings:',self.export_btn)
+
+        buttons = QHBoxLayout()
+        buttons.addWidget(self.save_btn)
+        buttons.addWidget(self.cancel_btn)
+
+        layout.addRow('',buttons)
+
         self.setLayout(layout)
 
     def setup_window(self):
         self.setWindowTitle('Settings')
-        self.setFixedSize(300,160)
+        self.setFixedSize(370,250)
 
     def create_link(self):
         self.save_btn.clicked.connect(self.submit_values)
